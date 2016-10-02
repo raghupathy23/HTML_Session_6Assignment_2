36565c3bdb42945edbee6d01ae1bec348817ff05
raghupathy23 <raghupathy0123@gmail.com>
2016-10-02 19:43:10 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+	var a = 1120;
+	var b = 4450;
+	var c = 430;
+	if(a > b && a > c) {
+		console.log("A is Greater than B & C : " +a);
+	}
+	else if(b > a && b > c){
+		console.log("B is Greater than A & C : " +b);
+	}
+	else { 
+		console.log("C is Greater than A & B : " +c);
+	}
