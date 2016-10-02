bb43e48bac26338ab0a83c478a1850157a85cbb2
raghupathy23 <raghupathy0123@gmail.com>
2016-10-02 19:46:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 	var a = 1120;
 	var b = 4450;
 	var c = 430;
+	// Using AND Logical Operator - returns true if both operands are true; otherwise, returns false
 	if(a > b && a > c) {
-		console.log("A is Greater than B & C : " +a);
+		console.log("A is Greater than B&C : " +a);
 	}
 	else if(b > a && b > c){
-		console.log("B is Greater than A & C : " +b);
+		console.log("B is Greater than A&C : " +b);
 	}
 	else { 
-		console.log("C is Greater than A & B : " +c);
+		console.log("C is Greater than A&B : " +c);
 	}
